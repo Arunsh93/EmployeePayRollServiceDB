58a051fb69e9a04e9f10c7ddd3b6db7f76465f74
Arunsh93 <Arunhosur93@gmail.com>
2021-10-25 17:11:16 +0530

    Create Employee payroll table

--- EmployeePayroll_SQLQuery3.sql
+++ EmployeePayroll_SQLQuery3.sql
@@ -1,3 +1,10 @@
 Create Database Payroll_Services;
 use Payroll_Services;
 
+Create Table Employee_Payroll
+(
+ Id int Identity(1,1) primary key,
+ EmpName varchar(100) Not null,
+ Salary Money Not null,
+ StartDate Date Not null
+ ); 
